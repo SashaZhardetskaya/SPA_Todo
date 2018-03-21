fb2cea01440ab38e8a38792347b5dfadcb7b86ce
sasha_zhardetskaya <sasha310209@gmail.com>
2018-03-21 13:16:52 +0200

    basic server setup

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const server = app.listen(8080, () => {
+    console.log(`Server is up and running on port 8080`);
+});
