bcedd09011c71056b89e48ca6ee8b9ac55b3452b
sasha_zhardetskaya <sasha310209@gmail.com>
2018-03-21 16:18:12 +0200

    add all roads and connected server with db

--- server/app.js
+++ server/app.js
@@ -1,12 +1,38 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
 
+import { serverPort } from '../etc/config.json';
 
+import * as db from './utils/DataBaseUtils';
+
+// Initialization of express application
 const app = express();
 
-app.get('/', (req, res) => {
+// Set up connection of database
+db.setUpConnection();
+
+// Using bodyParser middleware
+app.use( bodyParser.json() );
+
+
+// RESTful api handlers
+
+app.get('/hi', (req, res) => {
     res.send('Hello World!');
 });
 
-const server = app.listen(8080, () => {
-    console.log(`Server is up and running on port 8080`);
+app.get('/notes', (req, res) => {
+    db.listNotes().then(data => res.send(data));
+});
+
+app.post('/notes', (req, res) => {
+    db.createNote(req.body).then(data => res.send(data));
+});
+
+app.delete('/notes/:id', (req, res) => {
+    db.deleteNote(req.params.id).then(data => res.send(data));
+});
+
+const server = app.listen(serverPort, function() {
+    console.log(`Server is up and running on port ${serverPort}`);
 });
